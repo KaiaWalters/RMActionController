2e838f7a3fbae227e7d51990e0c11971235f7225
KWalters12 <kwalters12@humana.com>
2020-06-16 11:37:41 -0400

    Add Package.swift file

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "RMActionController",
+	platforms: [
+		.iOS(.v8)
+    )
+	],
+    products: [
+        .library(
+            name: "RMActionController",
+            targets: ["RMActionController"])
+	],
+	targets: [
+		.target(
+			name: "RMActionController",
+			dependencies: [],
+			path: "RMActionController")
+	],
+	swiftLanguageVersions: [.v5]
+)
